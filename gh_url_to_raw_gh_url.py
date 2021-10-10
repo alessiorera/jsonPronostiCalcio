b371c798dffabfe2adfd8cc8d77860b07cfeb04e
alessiorera <74310263+alessiorera@users.noreply.github.com>
2021-10-10 11:28:47 +0200

    Create gh_url_to_raw_gh_url.py

---
+++ gh_url_to_raw_gh_url.py
@@ -0,0 +1,9 @@
+# il tuo link va qui
+link  =  "https://alessiorera.github.io/jsonPronostiCalcio"
+
+# nota: questo si interromperà se un repository/organizzazione o una sottocartella si chiama "blob" - sarebbe l'ideale per usare un'espressione regolare di fantasia
+# per essere più precisi qui
+#print ( link . replace ( "github.com" , "raw.githubusercontent.com" ). replace ( "/blob/" , "/" ))
+
+# link di output di esempio:
+# https://raw.githubusercontent.com/knightlab-analyses/qurro-mackerel-analysis/master/AnalysisOutput/qurro-plot.qzv

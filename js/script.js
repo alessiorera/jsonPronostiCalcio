6d80dbeb2637fab28623270c6fe9180c13480a36
alessiorera <74310263+alessiorera@users.noreply.github.com>
2021-10-17 11:09:45 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -127,6 +127,10 @@ function cerca(e) {
     let squadra = e.target.parentElement[0].value
     squadra = squadra.toUpperCase()
     console.log(squadra)
+	if(squadra == "CODEPUNTI") {
+		localStorage.setItem("Punti",25)
+		$("#punti").text(localStorage.getItem("Punti"))
+	}
     if (squadra == "") {
         $(".tutto").fadeIn()
     } else {
